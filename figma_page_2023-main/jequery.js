5fabdcc0341e3d8c44d46e04e48754cfa71750cf
Krishan Chakroborty <138010864+krisHChakroborty@users.noreply.github.com>
2023-10-12 13:09:12 +0530

    first commit

---
+++ figma_page_2023-main/jequery.js
@@ -0,0 +1,34 @@
+
+$(document).ready(function() {
+
+
+
+
+    $(".navbar-toggler").click(function() {
+
+
+        $(".social2").toggle( function(){
+           
+        }); // This will hide the navbar with the id "navbar"
+
+
+    });
+
+
+
+
+
+    
+    $(".navbar-toggler").click(function(){
+
+
+        $(".data1").toggle();
+           
+       
+
+
+    });
+
+
+
+});
